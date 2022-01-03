a773f03c41311e17908210789ad7bccde8c11533
GesuYaro <shagiev.02@gmail.com>
2022-01-03 20:39:26 +0300

    new structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import { HeaderComponent } from './login-page/header/header.component';
+import { LoginFormComponent } from './login-page/login-form/login-form.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, HeaderComponent, LoginFormComponent ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, MatTabsModule ],
+  declarations: [ AppComponent, HeaderComponent, LoginFormComponent, LoginPageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
